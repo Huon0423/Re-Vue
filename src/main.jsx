57d6b4591f1f5d435361f648d3d2314ea26c39a3
조윤정 <dbswjd1413@naver.com>
2024-05-22 09:42:56 +0900

    update: 저장..

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom"
-=======
->>>>>>> 18f629b038b1ea12dc6afe95ddb8f9380fa93fb4
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
   <BrowserRouter>
     <App />
   </BrowserRouter>
-=======
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
->>>>>>> 18f629b038b1ea12dc6afe95ddb8f9380fa93fb4
 )
